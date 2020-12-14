247c6ecf4d43ebb10046c76e50ffc611079268bf
AAGrigoriev <grigormaklaud@gmail.com>
2020-12-14 17:33:59 +0300

    add cmake and using

--- inc/matrix.hpp
+++ inc/matrix.hpp
@@ -2,111 +2,117 @@
 #include <unordered_map>
 #include "hashFunction.hpp"
 
-template <typename T, T def_v,typename  std::enable_if<std::is_integral<T>::value>::type>
-class Matrix
+namespace OtusMatrix
 {
-private:
-    class Container_Wrapper
+    template <typename T, T def_v, typename = typename std::enable_if<std::is_integral<T>::value>::type>
+    class Matrix
     {
-    public:
-    
-        Container_Wrapper() = default;
-        ~Container_Wrapper() = default;
+        using size_matrix = typename std::unordered_map<MyKey, T, MyHashFunction>::size_type;
 
-        /*!
+    private:
+        class Container_Wrapper
+        {
+        public:
+            Container_Wrapper() = default;
+            ~Container_Wrapper() = default;
+
+            /*!
             @brief 
         */
-        T get(std::size_t row_index, std::size_t col_index)
-        {
-            auto search = u_map.find(MyKey(row_index, col_index));
-            return search == u_map.end() ? def_v : search->second;
-        }
+            T get(std::size_t row_index, std::size_t col_index)
+            {
+                auto search = u_map.find(MyKey(row_index, col_index));
+                return search == u_map.end() ? def_v : search->second;
+            }
 
-        /*!
+            /*!
             @brief Function assign value to unordered_map 
         */
-        void assign(std::size_t row_index, std::size_t col_index, T value)
-        {
-            if (value != def_v)
+            void assign(std::size_t row_index, std::size_t col_index, T value)
             {
-                u_map[MyKey(row_index, col_index)] = value;
+                if (value != def_v)
+                {
+                    u_map[MyKey(row_index, col_index)] = value;
+                }
+                else
+                {
+                    u_map.erase(MyKey(row_index, col_index));
+                }
             }
-            else
+
+            size_matrix getSize()
             {
-                u_map.erase(MyKey(row_index, col_index));
+                return u_map.size();
             }
-        }
 
-        std::unordered_map<MyKey, T, MyHashFunction>::size_type getSize()
+        private:
+            std::unordered_map<MyKey, T, MyHashFunction> u_map;
+        };
+
+        class ProxyCol
         {
-            return u_map.size();
-        }
+        public:
+            ProxyCol(std::size_t index_row, std::size_t index_col, Container_Wrapper &container) : index_row(index_row), index_col(index_col), container(container) {}
 
-    private:
-        std::unordered_map<MyKey, T, MyHashFunction> u_map;
-    };
+            ProxyCol &operator=(T value)
+            {
+                container.assign(index_row, index_col, value);
+            }
 
-    class ProxyCol
-    {
+            operator T()
+            {
+                return container.get(index_row, index_col);
+            }
+
+            ~ProxyCol() = default;
+            ProxyCol(ProxyCol const &other) = delete;
+            ProxyCol(ProxyCol &&other) = delete;
+            ProxyCol &operator=(ProxyCol const &other) = delete;
+            ProxyCol &operator=(ProxyCol &&other) = delete;
+
+        private:
+            std::size_t index_row;
+            std::size_t index_col;
+            Container_Wrapper &container;
+        };
+
+        class ProxyRow
+        {
+        public:
+            ProxyRow(std::size_t index_row, Container_Wrapper &container) : index_row(index_row), container(container) {}
+
+            ProxyCol operator[](std::size_t index_column)
+            {
+                return ProxyCol(index_row, index_column, container);
+            }
+
+            ~ProxyRow() = default;
+            ProxyRow(ProxyRow const &other) = delete;
+            ProxyRow(ProxyRow &&other) = delete;
+            ProxyRow &operator=(ProxyRow const &other) = delete;
+            ProxyRow &operator=(ProxyRow &&other) = delete;
+
+        private:
+            std::size_t index_row;
+            Container_Wrapper &container;
+        };
+
     public:
-        ProxyCol(std::size_t index_row, std::size_t index_col, Container_Wrapper &container) : index_row(index_row), index_col(index_col), container(container) {}
+        Matrix() = default;
+        ~Matrix() = default;
 
-        ProxyCol &operator=(T value)
+        ProxyRow operator[](std::size_t index_row)
         {
-            container.assign(index_row, index_col, value);
+            return ProxyRow(index_row);
         }
 
-        operator T()
+        size_matrix getSize()
         {
-            return container.get(index_row, index_col);
+            return container.getSize();
         }
-
-        ~ProxyCol() = default;
-        ProxyCol(ProxyCol const &other) = delete;
-        ProxyCol(ProxyCol &&other) = delete;
-        ProxyCol &operator=(ProxyCol const &other) = delete;
-        ProxyCol &operator=(ProxyCol &&other) = delete;
 
     private:
-        std::size_t index_row;
-        std::size_t index_col;
-        Container_Wrapper &container;
+        Container_Wrapper container;
     };
 
-    class ProxyRow
-    {
-    public:
-        ProxyRow(std::size_t index_row, Container_Wrapper &container) : index_row(index_row), container(container) {}
-
-        ProxyCol operator[](std::size_t index_column)
-        {
-            return ProxyCol(index_row, index_column, container);
-        }
-
-        ~ProxyRow() = default;
-        ProxyRow(ProxyRow const &other) = delete;
-        ProxyRow(ProxyRow &&other) = delete;
-        ProxyRow &operator=(ProxyRow const &other) = delete;
-        ProxyRow &operator=(ProxyRow &&other) = delete;
-
-    private:
-        std::size_t index_row;
-        Container_Wrapper &container;
-    };
-
-public:
-    Matrix() = default;
-
-    ProxyRow operator[](std::size_t index_row)
-    {
-        return ProxyRow(index_row);
-    }
-
-    std::unordered_map<MyKey, T, MyHashFunction>::size_type getSize()
-    {
-        return container.getSize();
-    }
-
-private:
-    Container_Wrapper container;
-};
+} // namespace OtusMatrix
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "matrix.hpp"
+
+int main()
+{
+
+    OtusMatrix::Matrix<int,-1> matrix;
+
+    return 0;
+}
